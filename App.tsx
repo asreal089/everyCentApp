74c42ec23763c9d4fd2d00185b4194766a00f364
asreal089 <lexx_89@hotmail.com>
2023-04-29 22:44:01 -0300

    Cleaning main window.

--- App.tsx
+++ App.tsx
@@ -1,84 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {  } from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import { Header } from './src/components/Header';
 import { Footer } from './src/components/Footer';
-import LoadingScreen from './src/screens/LoadingScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate app initialization
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }, []);
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      {isLoading ? (
-        <LoadingScreen />
-      ) :(
-
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-      )}
-    </View>
-  );
-}
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView style={styles.backgroundStyle}>
+      
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+        style={styles.backgroundStyle}>
           <Header subTitle={'Home'} />
         <View style={styles.sectionContainer}>
-          <Section title="Every cent">Your budgeting App</Section>
+          <Text >Your budgeting App</Text>
         </View>
         <Footer />
       </ScrollView>
@@ -87,6 +30,9 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
+  backgroundStyle: {
+    flex: 1,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
