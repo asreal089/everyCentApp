b348c19016dc8969aa28ab47e0e7d949d927df7d
asreal089 <lexx_89@hotmail.com>
2023-05-04 06:52:19 -0300

    Adding pie chart mock

--- src/screens/BudgetScreen.tsx
+++ src/screens/BudgetScreen.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import {StyleSheet, Text, SafeAreaView, ScrollView, View} from 'react-native';
+import {StyleSheet, Text, SafeAreaView, ScrollView, View, Dimensions} from 'react-native';
 import {BudgetItem} from '../components/BudgetItem';
 import {DataTable} from 'react-native-paper';
+import {
+  PieChart
+} from "react-native-chart-kit";
 
 const BudgetScreen = () => {
   const dataIncome = [
@@ -52,10 +55,68 @@ const BudgetScreen = () => {
       limitAmount: 50,
     },
   ];
+  const chartData = [
+    {
+      name: "Seoul",
+      population: 21500000,
+      color: "rgba(131, 167, 234, 1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Toronto",
+      population: 2800000,
+      color: "#F00",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Beijing",
+      population: 527612,
+      color: "red",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "New York",
+      population: 8538000,
+      color: "#ffffff",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Moscow",
+      population: 11920000,
+      color: "rgb(0, 0, 255)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    }
+  ];
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
   return (
     <SafeAreaView>
       <ScrollView>
         <View style={styles.container}>
+        <PieChart
+          data={chartData}
+          width={Dimensions.get("window").width}
+          height={220}
+          chartConfig={chartConfig}
+          accessor={"population"}
+          backgroundColor={"transparent"}
+          paddingLeft={"15"}
+          center={[10, 50]}
+          absolute
+        />
           <Text>Income:</Text>
           <DataTable>
             <DataTable.Header>
