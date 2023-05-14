29dd5ab2625cacb4d9d97f133994b14c62379fd8
asreal089 <lexx_89@hotmail.com>
2023-05-14 16:35:29 -0300

    Add expenses and income table.

--- src/screens/ExpensesScreen.tsx
+++ src/screens/ExpensesScreen.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import {DataTable} from 'react-native-paper';
+import formatMoney from '../utils/NumberUtils';
 
 interface Transaction {
   id: number;
@@ -75,8 +76,40 @@ const ExpensesScreen = () => {
           <DataTable.Cell numeric>{totalExpense}</DataTable.Cell>
         </DataTable.Row>
       </DataTable>
-
+      <Text>Income:</Text>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Name</DataTable.Title>
+          <DataTable.Title >Amount</DataTable.Title>
+          <DataTable.Title >Date</DataTable.Title>
+          <DataTable.Title>Category</DataTable.Title>
+        </DataTable.Header>
+        {incomes.map((income, index) => (
+          <DataTable.Row key={index}>
+            <DataTable.Cell>{income.name}</DataTable.Cell>
+            <DataTable.Cell>{formatMoney(income.amount)}</DataTable.Cell>
+            <DataTable.Cell >{income.date.getDay()+1+"-" + income.date.getMonth()+"-"+income.date.getFullYear()}</DataTable.Cell>
+            <DataTable.Cell>{income.category}</DataTable.Cell>
+          </DataTable.Row>
+        ))}
+      </DataTable>
       <Text>Expenses:</Text>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Name</DataTable.Title>
+          <DataTable.Title >Amount</DataTable.Title>
+          <DataTable.Title >Date</DataTable.Title>
+          <DataTable.Title>Category</DataTable.Title>
+        </DataTable.Header>
+        {expenses.map((expenses, index) => (
+          <DataTable.Row key={index}>
+            <DataTable.Cell>{expenses.name}</DataTable.Cell>
+            <DataTable.Cell>{formatMoney(expenses.amount)}</DataTable.Cell>
+            <DataTable.Cell >{expenses.date.getDay()+1+"-" + expenses.date.getMonth()+"-"+expenses.date.getFullYear()}</DataTable.Cell>
+            <DataTable.Cell>{expenses.category}</DataTable.Cell>
+          </DataTable.Row>
+        ))}
+      </DataTable>
     </View>
   );
 };
