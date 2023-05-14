6c913e08c9dd733923ae0df867e314a7b935a73d
asreal089 <lexx_89@hotmail.com>
2023-05-14 17:05:17 -0300

    Adding scroll view

--- src/screens/ExpensesScreen.tsx
+++ src/screens/ExpensesScreen.tsx
@@ -1,9 +1,16 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
 import {DataTable} from 'react-native-paper';
 import formatMoney from '../utils/NumberUtils';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { MyColors } from '../styles/Color';
+import {MyColors} from '../styles/Color';
+import {ScrollView} from 'react-native-gesture-handler';
 
 interface Transaction {
   id: number;
@@ -40,6 +47,46 @@ const ExpensesScreen = () => {
       category: 'Utilities',
       isIncome: false,
     },
+    {
+      id: 4,
+      name: 'Internet Bill',
+      amount: 60,
+      date: new Date('2023-05-04'),
+      category: 'Utilities',
+      isIncome: false,
+    },
+    {
+      id: 5,
+      name: 'Dinner Out',
+      amount: 50,
+      date: new Date('2023-05-05'),
+      category: 'Food',
+      isIncome: false,
+    },
+    {
+      id: 6,
+      name: 'Gasoline',
+      amount: 40,
+      date: new Date('2023-05-06'),
+      category: 'Transportation',
+      isIncome: false,
+    },
+    {
+      id: 7,
+      name: 'Movie Tickets',
+      amount: 25,
+      date: new Date('2023-05-07'),
+      category: 'Entertainment',
+      isIncome: false,
+    },
+    {
+      id: 8,
+      name: 'Phone Bill',
+      amount: 70,
+      date: new Date('2023-05-08'),
+      category: 'Utilities',
+      isIncome: false,
+    },
   ]);
   const [incomes, setIncomes] = useState<Transaction[]>([
     {
@@ -67,78 +114,104 @@ const ExpensesScreen = () => {
   );
 
   return (
-    <View style={styles.container}>
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title>Total income</DataTable.Title>
-          <DataTable.Title numeric>Total Expenses</DataTable.Title>
-        </DataTable.Header>
-        <DataTable.Row>
-          <DataTable.Cell>{totalIncome}</DataTable.Cell>
-          <DataTable.Cell numeric>{totalExpense}</DataTable.Cell>
-        </DataTable.Row>
-      </DataTable>
-      <Text>Income:</Text>
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title>Name</DataTable.Title>
-          <DataTable.Title >Amount</DataTable.Title>
-          <DataTable.Title >Date</DataTable.Title>
-          <DataTable.Title>Category</DataTable.Title>
-          <DataTable.Title numeric>Actions</DataTable.Title>
-        </DataTable.Header>
-        {incomes.map((income, index) => (
-          <DataTable.Row key={index}>
-            <DataTable.Cell>{income.name}</DataTable.Cell>
-            <DataTable.Cell>{formatMoney(income.amount)}</DataTable.Cell>
-            <DataTable.Cell >{income.date.getDay()+1+"-" + income.date.getMonth()+"-"+income.date.getFullYear()}</DataTable.Cell>
-            <DataTable.Cell>{income.category}</DataTable.Cell>
-            <DataTable.Cell numeric>
-            <TouchableOpacity
-              onPress={() => console.log("edit button")}>
-              <Icon
-                name="square-edit-outline"
-                color={MyColors.backgrounddefault}
-                size={25}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={()=> console.log("deleting budegt")}>
-              <Icon name="delete" color={MyColors.backgrounddefault} size={25} />
-            </TouchableOpacity>
-          </DataTable.Cell>
-          </DataTable.Row>
-        ))}
-      </DataTable>
-      <Text>Expenses:</Text>
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title>Name</DataTable.Title>
-          <DataTable.Title >Amount</DataTable.Title>
-          <DataTable.Title >Date</DataTable.Title>
-          <DataTable.Title>Category</DataTable.Title>
-          <DataTable.Title numeric>Actions</DataTable.Title>
-        </DataTable.Header>
-        {expenses.map((expenses, index) => (
-          <DataTable.Row key={index}>
-            <DataTable.Cell>{expenses.name}</DataTable.Cell>
-            <DataTable.Cell>{formatMoney(expenses.amount)}</DataTable.Cell>
-            <DataTable.Cell >{expenses.date.getDay()+1+"-" + expenses.date.getMonth()+"-"+expenses.date.getFullYear()}</DataTable.Cell>
-            <DataTable.Cell>{expenses.category}</DataTable.Cell>
-            <TouchableOpacity
-              onPress={() => console.log("edit button")}>
-              <Icon
-                name="square-edit-outline"
-                color={MyColors.backgrounddefault}
-                size={25}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={()=> console.log("deleting budegt")}>
-              <Icon name="delete" color={MyColors.backgrounddefault} size={25} />
-            </TouchableOpacity>
-          </DataTable.Row>
-        ))}
-      </DataTable>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <DataTable>
+            <DataTable.Header>
+              <DataTable.Title>Total income</DataTable.Title>
+              <DataTable.Title numeric>Total Expenses</DataTable.Title>
+            </DataTable.Header>
+            <DataTable.Row>
+              <DataTable.Cell>{totalIncome}</DataTable.Cell>
+              <DataTable.Cell numeric>{totalExpense}</DataTable.Cell>
+            </DataTable.Row>
+          </DataTable>
+          <Text>Income:</Text>
+          <DataTable>
+            <DataTable.Header>
+              <DataTable.Title>Name</DataTable.Title>
+              <DataTable.Title>Amount</DataTable.Title>
+              <DataTable.Title>Date</DataTable.Title>
+              <DataTable.Title>Category</DataTable.Title>
+              <DataTable.Title numeric>Actions</DataTable.Title>
+            </DataTable.Header>
+            {incomes.map((income, index) => (
+              <DataTable.Row key={index}>
+                <DataTable.Cell>{income.name}</DataTable.Cell>
+                <DataTable.Cell>{formatMoney(income.amount)}</DataTable.Cell>
+                <DataTable.Cell>
+                  {income.date.getDay() +
+                    1 +
+                    '-' +
+                    income.date.getMonth() +
+                    '-' +
+                    income.date.getFullYear()}
+                </DataTable.Cell>
+                <DataTable.Cell>{income.category}</DataTable.Cell>
+                <DataTable.Cell numeric>
+                  <TouchableOpacity onPress={() => console.log('edit button')}>
+                    <Icon
+                      name="square-edit-outline"
+                      color={MyColors.backgrounddefault}
+                      size={25}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => console.log('deleting budegt')}>
+                    <Icon
+                      name="delete"
+                      color={MyColors.backgrounddefault}
+                      size={25}
+                    />
+                  </TouchableOpacity>
+                </DataTable.Cell>
+              </DataTable.Row>
+            ))}
+          </DataTable>
+          <Text>Expenses:</Text>
+          <DataTable>
+            <DataTable.Header>
+              <DataTable.Title>Name</DataTable.Title>
+              <DataTable.Title>Amount</DataTable.Title>
+              <DataTable.Title>Date</DataTable.Title>
+              <DataTable.Title>Category</DataTable.Title>
+              <DataTable.Title numeric>Actions</DataTable.Title>
+            </DataTable.Header>
+            {expenses.map((expenses, index) => (
+              <DataTable.Row key={index}>
+                <DataTable.Cell>{expenses.name}</DataTable.Cell>
+                <DataTable.Cell>{formatMoney(expenses.amount)}</DataTable.Cell>
+                <DataTable.Cell>
+                  {expenses.date.getDay() +
+                    1 +
+                    '-' +
+                    expenses.date.getMonth() +
+                    '-' +
+                    expenses.date.getFullYear()}
+                </DataTable.Cell>
+                <DataTable.Cell>{expenses.category}</DataTable.Cell>
+                <TouchableOpacity onPress={() => console.log('edit button')}>
+                  <Icon
+                    name="square-edit-outline"
+                    color={MyColors.backgrounddefault}
+                    size={25}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => console.log('deleting budegt')}>
+                  <Icon
+                    name="delete"
+                    color={MyColors.backgrounddefault}
+                    size={25}
+                  />
+                </TouchableOpacity>
+              </DataTable.Row>
+            ))}
+          </DataTable>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
