ca47be250885c24cc26892cb6a0db018d9ae9e1e
asreal089 <lexx_89@hotmail.com>
2023-05-22 08:40:03 -0300

    Adding barchat im home dashboard

--- src/screens/HomeScree.tsx
+++ src/screens/HomeScree.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {View, StyleSheet, Text, useWindowDimensions} from 'react-native';
 import {ChartColors, MyColors} from '../styles/Color';
 import ProgressChart from 'react-native-chart-kit/dist/ProgressChart';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ScrollView} from 'react-native-gesture-handler';
+import {BarChart} from 'react-native-chart-kit';
+import { ChartData } from 'react-native-chart-kit/dist/HelperTypes';
 
 const expenses = [
   {
@@ -26,9 +30,21 @@ const expenses = [
   },
 ];
 
-const chartData: {labels: any, data:any, colors:any} = { labels: [], data: [], colors: [] };
+const chartData: {labels: any; data: any; colors: any} = {
+  labels: [],
+  data: [],
+  colors: [],
+};
+
+const barChartData : ChartData = {
+  labels: [],
+  datasets: []
+};
+
 expenses.forEach((item, index) => {
   chartData.labels.push(item.category);
+  barChartData.labels.push(item.category);
+  barChartData.datasets.push({data: [item.expense, item.budget]});
   chartData.data.push(item.expense / item.budget);
 });
 const chartConfig = {
@@ -42,23 +58,44 @@ const chartConfig = {
     borderRadius: 1,
   },
   strokeWidth: 4,
-  
 };
+
+
+
+
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <ProgressChart
-        data={chartData}
-        width={useWindowDimensions().width}
-        height={220}
-        strokeWidth={16}
-        radius={32}
-        chartConfig={chartConfig}
-        hideLegend={false}
-      />
-      <Text></Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text>Current progress:</Text>
+          <ProgressChart
+            data={chartData}
+            width={useWindowDimensions().width}
+            height={220}
+            strokeWidth={16}
+            radius={32}
+            chartConfig={chartConfig}
+            hideLegend={false}
+          />
+          <Text></Text>
+          <BarChart
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+            data={barChartData}
+            width={useWindowDimensions().width}
+            height={220}
+            yAxisLabel="$"
+            chartConfig={chartConfig}
+            verticalLabelRotation={30}
+            yAxisSuffix="$"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
